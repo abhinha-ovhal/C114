64e78dd33f76893948deaf077d112221cf8e21a2
abhinha-ovhal <133642974+abhinha-ovhal@users.noreply.github.com>
2023-12-16 20:05:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preLoad(){
+
+}
+
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.center();
+}
+
+function draw(){
+
+}
+
+function snap(){
+    save("filter-image.png");
+}
